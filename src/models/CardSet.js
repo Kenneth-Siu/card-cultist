dfe6615a95c50b69191ea152e385ef0126be5e03
Kenneth Siu <kennethsiu11@gmail.com>
2022-09-08 17:59:47 +0100

    Fix bug when creating new card sets

--- src/models/CardSet.js
+++ src/models/CardSet.js
@@ -4,13 +4,13 @@ import Card from "./card";
 export default class CardSet {
     constructor(idOrCardSet) {
         if (typeof idOrCardSet === "number") {
-            this.id = id;
+            this.id = idOrCardSet;
             this.title = "";
             this.symbol = null;
             this.cards = [];
         }
         Object.assign(this, idOrCardSet);
-        this.cards = idOrCardSet.cards.map(card => new Card(card));
+        this.cards = this.cards.map(card => new Card(card));
     }
 
     addCard() {
