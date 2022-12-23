6b07294f487429693fb9e49128d94d8c55cdd59a
Kenneth Siu <Kenneth.Siu@softwire.com>
2022-12-23 10:41:30 +0000

    Add teutonic font

--- src/pages/App.js
+++ src/pages/App.js
@@ -32,6 +32,7 @@ export default function App() {
             <p style={{ fontFamily: "AHCardTextSymbols", color: "transparent", position: "fixed", top: "-100px" }}>a</p>
             <p style={{ fontFamily: "NotoColorEmoji-Regular", color: "transparent", position: "fixed", top: "-100px" }}>📚</p>
             <p style={{ fontFamily: "Arno", color: "transparent", position: "fixed", top: "-100px" }}>a</p>
+            <p style={{ fontFamily: "Teutonic", color: "transparent", position: "fixed", top: "-100px" }}>a</p>
             <Switch>
                 <Route exact={true} path="/campaign-guide">
                     <CampaignGuideView campaign={campaign} setCampaign={setCampaign} />
