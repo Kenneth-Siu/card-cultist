75bde4f10e6adbaf8e2e6070f37be1ec7fcb59d5
Kenneth Siu <kennethsiu11@gmail.com>
2022-12-27 20:59:45 +0000

    Extract sortable encounter sets list

--- src/pages/campaign/CampaignView.js
+++ src/pages/campaign/CampaignView.js
@@ -1,17 +1,12 @@
-import { DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
-import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
-import { DragIndicator } from "@mui/icons-material";
 import React, { useContext, useEffect, useState } from "react";
 import { CampaignContext } from "../../components/CampaignContext";
+import SortableEncounterSetsList from "./components/SortableEncounterSetsList";
 import Container from "../../components/container/Container";
 import useLoadedImages from "../../helpers/useLoadedImages";
 import Campaign from "./Campaign";
 import "./CampaignView.scss";
 
 export default function CampaignView() {
-    const sensors = useSensors(useSensor(PointerSensor));
-
     const { campaign, refreshCampaign } = useContext(CampaignContext);
     const [newCampaignTitle, setNewCampaignTitle] = useState("");
 
@@ -32,24 +27,7 @@ export default function CampaignView() {
                         <button onClick={() => setCampaignSymbol()}>Choose campaign symbol</button>
                         {campaign.symbol && loadedImages.length > 0 ? loadedImages[loadedImages.length - 1] : ""}
                     </Container>
-                    <Container className="encounter-sets-list-container">
-                        <button>Generate campaign card numbers</button>
-                        <table>
-                            <tbody>
-                                <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
-                                    <SortableContext items={campaign.cardSets} strategy={verticalListSortingStrategy}>
-                                        {campaign.cardSets.map((cardSet) => (
-                                            <SortableEncounterSetRow
-                                                cardSet={cardSet}
-                                                key={cardSet.id}
-                                                id={cardSet.id}
-                                            />
-                                        ))}
-                                    </SortableContext>
-                                </DndContext>
-                            </tbody>
-                        </table>
-                    </Container>
+                    <SortableEncounterSetsList />
                 </>
             ) : (
                 <div>
@@ -94,31 +72,4 @@ export default function CampaignView() {
         campaign.symbol = path;
         refreshCampaign();
     }
-
-    function handleDragEnd(event) {
-        const { active, over } = event;
-
-        if (active.id !== over.id) {
-            const oldIndex = campaign.cardSets.findIndex((cardSet) => cardSet.id === active.id);
-            const newIndex = campaign.cardSets.findIndex((cardSet) => cardSet.id === over.id);
-            campaign.cardSets = arrayMove(campaign.cardSets, oldIndex, newIndex);
-            refreshCampaign();
-        }
-    }
-}
-
-function SortableEncounterSetRow({ cardSet, id }) {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
-    const style = {
-        transform: CSS.Transform.toString(transform),
-        transition,
-    };
-    return (
-        <tr ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <td className="drag-indicator">
-                <DragIndicator />
-            </td>
-            <td>{cardSet.title}</td>
-        </tr>
-    );
 }
---
+++ src/pages/campaign/components/SortableEncounterSetsList.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import Container from "../../../components/container/Container";
+import { DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { DragIndicator } from "@mui/icons-material";
+import { CampaignContext } from "../../../components/CampaignContext";
+
+export default function SortableEncounterSetsList() {
+    const { campaign, refreshCampaign } = useContext(CampaignContext);
+    const sensors = useSensors(useSensor(PointerSensor));
+
+    return (
+        <Container className="encounter-sets-list-container">
+            <button>Generate campaign card numbers</button>
+            <table>
+                <tbody>
+                    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+                        <SortableContext items={campaign.cardSets} strategy={verticalListSortingStrategy}>
+                            {campaign.cardSets.map((cardSet) => (
+                                <SortableEncounterSetRow cardSet={cardSet} key={cardSet.id} id={cardSet.id} />
+                            ))}
+                        </SortableContext>
+                    </DndContext>
+                </tbody>
+            </table>
+        </Container>
+    );
+
+    function handleDragEnd(event) {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            const oldIndex = campaign.cardSets.findIndex((cardSet) => cardSet.id === active.id);
+            const newIndex = campaign.cardSets.findIndex((cardSet) => cardSet.id === over.id);
+            campaign.cardSets = arrayMove(campaign.cardSets, oldIndex, newIndex);
+            refreshCampaign();
+        }
+    }
+}
+
+function SortableEncounterSetRow({ cardSet, id }) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    };
+    return (
+        <tr ref={setNodeRef} style={style} {...attributes} {...listeners}>
+            <td className="drag-indicator">
+                <DragIndicator />
+            </td>
+            <td>{cardSet.title}</td>
+        </tr>
+    );
+}
