4f739b033bd8cbbcd99b6d7a2c0f4de5f379e8e9
Kenneth Siu <kennethsiu11@gmail.com>
2022-12-27 20:34:44 +0000

    Can manually sort card list

--- src/pages/cardSet/components/SortableCardList.js
+++ src/pages/cardSet/components/SortableCardList.js
@@ -2,10 +2,14 @@ import React, { useContext } from "react";
 import { CampaignContext } from "../../../components/CampaignContext";
 import DragIndicator from "@mui/icons-material/DragIndicator";
 import Container from "../../../components/container/Container";
+import { CSS } from "@dnd-kit/utilities";
 import "./SortableCardList.scss";
+import { DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
 
 export default function SortableCardList({ cardSet }) {
     const { refreshCampaign } = useContext(CampaignContext);
+    const sensors = useSensors(useSensor(PointerSensor));
 
     return (
         <Container className="sortable-card-list-container">
@@ -22,35 +26,13 @@ export default function SortableCardList({ cardSet }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {cardSet.cards.map((card) => (
-                        <tr key={card.id}>
-                            <td className="drag-indicator">
-                                <DragIndicator />
-                            </td>
-                            <td>{card.getTitle()}</td>
-                            <td>
-                                <span className="emoji">{card.getEmoji()}</span> {card.frontFace.type}
-                            </td>
-                            <td>{card.numOfCopies}</td>
-                            <td>
-                                {card.frontFace.encounterSetId || card.frontFace.encounterSetMaxId
-                                    ? card.frontFace.encounterSetId +
-                                      String.fromCharCode(8202) +
-                                      "/" +
-                                      String.fromCharCode(8202) +
-                                      card.frontFace.encounterSetMaxId
-                                    : ""}{" "}
-                                {hasInconsistentEncounterSetIds(card) && (
-                                    <span
-                                        className="emoji"
-                                        title="Front and back faces have different encounter set card numbers"
-                                    >
-                                        ⚠
-                                    </span>
-                                )}
-                            </td>
-                        </tr>
-                    ))}
+                    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+                        <SortableContext items={cardSet.cards} strategy={verticalListSortingStrategy}>
+                            {cardSet.cards.map((card) => (
+                                <SortableCardRow card={card} key={card.id} id={card.id} />
+                            ))}
+                        </SortableContext>
+                    </DndContext>
                 </tbody>
             </table>
         </Container>
@@ -65,6 +47,52 @@ export default function SortableCardList({ cardSet }) {
         cardSet.generateEncounterSetCardNumbers();
         refreshCampaign();
     }
+
+    function handleDragEnd(event) {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            const oldIndex = cardSet.cards.findIndex((card) => card.id === active.id);
+            const newIndex = cardSet.cards.findIndex((card) => card.id === over.id);
+            console.log(oldIndex, newIndex);
+            cardSet.cards = arrayMove(cardSet.cards, oldIndex, newIndex);
+            refreshCampaign();
+        }
+    }
+}
+
+function SortableCardRow({ card, id }) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    };
+    return (
+        <tr ref={setNodeRef} style={style} {...attributes} {...listeners}>
+            <td className="drag-indicator">
+                <DragIndicator />
+            </td>
+            <td>{card.getTitle()}</td>
+            <td>
+                <span className="emoji">{card.getEmoji()}</span> {card.frontFace.type}
+            </td>
+            <td>{card.numOfCopies}</td>
+            <td>
+                {card.frontFace.encounterSetId || card.frontFace.encounterSetMaxId
+                    ? card.frontFace.encounterSetId +
+                      String.fromCharCode(8202) +
+                      "/" +
+                      String.fromCharCode(8202) +
+                      card.frontFace.encounterSetMaxId
+                    : ""}{" "}
+                {hasInconsistentEncounterSetIds(card) && (
+                    <span className="emoji" title="Front and back faces have different encounter set card numbers">
+                        ⚠
+                    </span>
+                )}
+            </td>
+        </tr>
+    );
 }
 
 function hasInconsistentEncounterSetIds(card) {
