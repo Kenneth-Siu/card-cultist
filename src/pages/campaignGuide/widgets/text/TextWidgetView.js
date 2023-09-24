8b2a9ca31f02954050e9b152031c8bc4dcdc1704
Kenneth Siu <kennethsiu11@gmail.com>
2023-09-24 17:45:36 +0100

    Debounce text widgets on campaign guide

--- src/pages/campaignGuide/widgets/text/TextWidgetView.js
+++ src/pages/campaignGuide/widgets/text/TextWidgetView.js
@@ -1,17 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CampaignContext } from "../../../../components/CampaignContext";
 import WidgetView from "../WidgetView";
+import useDebounce from "../../../../helpers/useDebounce";
 
 export default function TextWidgetView({ widget, page }) {
+    const [text, setText] = useState(widget.text);
     const { refreshCampaign } = useContext(CampaignContext);
-    return (
-        <WidgetView widget={widget} page={page} className="text-widget-view">
-            <textarea value={widget.text} onChange={(event) => setText(event.target.value)} />
-        </WidgetView>
-    );
 
-    function setText(text) {
+    const debouncedUpdate = useDebounce(() => {
         widget.text = text;
         refreshCampaign();
-    }
+    });
+
+    const updateText = (newText) => {
+        setText(newText);
+        debouncedUpdate();
+    };
+
+    return (
+        <WidgetView widget={widget} page={page} className="text-widget-view">
+            <textarea value={text} onChange={(event) => updateText(event.target.value)} />
+        </WidgetView>
+    );
 }
